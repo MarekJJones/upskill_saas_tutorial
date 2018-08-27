25c70f59d77ad79938c8bb7b9168b03509528f7a
Marek Jones <MarekJJones@gmail.com>
2018-08-27 15:03:04 +0000

    Added contact from mailer view

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -4,7 +4,7 @@ class ContactMailer < ActionMailer::Base
   def contact_email(name, email, body)
     @name = name
     @email = email
-    @bpdy = body
+    @body = body
     
     mail(from: email, subject: 'Contact From Message')
   end
