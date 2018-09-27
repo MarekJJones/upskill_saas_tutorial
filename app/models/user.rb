70935ce16c650b3107a058a2fc86aa367cd16bb5
Marek Jones <MarekJJones@gmail.com>
2018-09-27 21:36:39 +0000

    Added Stripe JS library and pro form id

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,23 +1,26 @@
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  
+         
   belongs_to :plan
   has_one :profile
   
   attr_accessor :stripe_card_token
-  # If Pro user passes validationg (email, passwords, etc),
-  # then call Stripe and tells Stripe to set up a subscription
-  # upon charging the customer's card
-  # Stripe responds back with customer data.
+  # If Pro user passes validations (email, password, etc.),
+  # then call Stripe and tell Stripe to set up a subscription
+  # upon charging the customer's card. 
+  # Stripe responds back with customer data. 
   # Store customer.id as the customer token and save the user.
-  def save_with_subscription
+attr_accessor :stripe_card_token
+
+  def save_with_payment
     if valid?
-      customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
+      customer = Stripe::Customer.create(description: email, plan: plan_id, source: stripe_card_token)
       self.stripe_customer_token = customer.id
-      save!
+     save!
     end
   end
 end
